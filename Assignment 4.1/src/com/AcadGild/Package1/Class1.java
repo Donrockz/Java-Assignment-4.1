15515bd645b842a52f71277a4bfa15b2cd692a0f
Donrockz <rockstarrockey18@gmail.com>
2017-06-10 03:06:39 +0530

    Add files via upload

---
+++ Assignment 4.1/src/com/AcadGild/Package1/Class1.java
@@ -0,0 +1,17 @@
+package com.AcadGild.Package1;
+
+import com.AcadGild.Package2.Class2;;
+
+public class Class1 {
+
+public static void main(String[] args) {
+	
+	Class2.main(args);
+	
+	
+	
+}
+		
+		
+	}
+
---
+++ Assignment 4.1/src/com/AcadGild/Package2/Class2.java
@@ -0,0 +1,14 @@
+package com.AcadGild.Package2;
+
+public class Class2 {
+	
+	
+
+	public static void main(String[] args) {
+		
+		int a = 12;
+		int b = 7;
+		int sum = a+b;
+		System.out.println(sum);
+	}
+}
